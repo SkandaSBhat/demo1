1d3406c97faa4f16c6eaedffd7f5b033d4b5708e
skanda s bhat <skandabhat21@gmail.com>
2023-03-20 23:01:11 +0530

    commiting two file

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.util. *;
+class Hello
+{
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    String s1 = s.nextLine();
+    System.out.println("Please enter your name: " + s1);
+
+  }
+}
